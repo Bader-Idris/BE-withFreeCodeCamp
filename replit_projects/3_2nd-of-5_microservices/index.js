7c1aa777c337995c77f894b0092ab216eaa46923
Bader Idris <baidsforbusiness@gmail.com>
2023-10-04 11:13:17 +0300

    finished 2nd, preparing to deploy it

--- replit_projects/3_2nd-of-5_microservices/index.js
+++ replit_projects/3_2nd-of-5_microservices/index.js
@@ -1,30 +1,28 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
+const express = require('express');
+const app = express();
+const myRouter = require('./myRouter')
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.use('/api/', myRouter);
+
+app.get('*', function (req, res) {
+  res.send(
+    '<pre>not found</pre>' +
+    `<style>
+      body {background-color: #121212;
+      color: #fff;
+    }</style>`
+  );
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
